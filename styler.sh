34c333b746bf0356e1243b280051c8a1d68dd111
Rinigus <rinigus.git@gmail.com>
2017-12-31 12:30:03 +0200

    [wip] styles with variables

--- styler.sh
+++ styler.sh
@@ -2,16 +2,21 @@
 
 set -e
 
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <styledir> <outputdir> [extra options for variable replacer]"
+    exit -1
+fi
+
 PATH=`npm bin`:$PATH
 
-styledir=$1
+styledir=${1%/}
 outdir=$2
 sname=$outdir/`basename $styledir`.json
 
 vname=`mktemp --suffix=.json`
 
 lessc --variables-output=$vname $styledir/variables.less > /dev/null
-variable-replacer --data=$vname $styledir/style.json $sname
+variable-replacer --data=$vname $styledir/style.json $sname "${@:3}"
 
 rm $vname
 echo "Style written to $sname"
